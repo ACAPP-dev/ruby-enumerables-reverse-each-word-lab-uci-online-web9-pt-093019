ad984a40dd739f63b15d6e8b766157bd80c3b94b
Andrew Capp <acapp909@gmail.com>
2019-09-26 08:43:20 +0000

    Automatically backed up by Learn

--- rev-practice.rb
+++ rev-practice.rb
@@ -16,7 +16,7 @@ def reverse_each_word2(str)
   p array1
    array1.collect {|item| array_rev.push(item.reverse)}
   #p array1
-  p array_rev
+  p array_rev.join(" ")
 end
 
 p reverse_each_word2("Hi again, just making sure it's reversed!")
