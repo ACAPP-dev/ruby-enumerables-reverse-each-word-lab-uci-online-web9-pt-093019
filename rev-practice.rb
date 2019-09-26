62bdaebdea0d613ecd0aa3b4d76d36d4961da2a9
Andrew Capp <acapp909@gmail.com>
2019-09-26 08:31:08 +0000

    Automatically backed up by Learn

--- rev-practice.rb
+++ rev-practice.rb
@@ -2,6 +2,10 @@ def reverse_each_word(str)
   array_rev = []
   array1 = str.split(/ /)
   array1.each {|item| array_rev.push(item.reverse)}
-  array_rev.to_s(array_rev)
-  return array_rev
+  array_rev.to_s
+p array_rev
 end
+
+
+
+reverse_each_word("Hello there, and how are you?")
