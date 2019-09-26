6501e314ddbc9eb1cfbaa529d6ab988390950773
Andrew Capp <acapp909@gmail.com>
2019-09-26 08:33:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(str)
   array_rev = []
   array1 = str.split(/ /)
   array1.each {|item| array_rev.push(item.reverse)}
-  array_rev.join(array_rev)
+  array_rev = array_rev.join
   return array_rev
 end
